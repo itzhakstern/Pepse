ec3c85219fa8f795f0c6cdce21b7eaba8317cc39
david96 <rotenberg.david@mail.huji.ac.il>
2022-01-04 14:43:20 +0200

    add class

--- src/Pepse/world/Terrain.java
+++ src/Pepse/world/Terrain.java
@@ -1,12 +1,17 @@
 package Pepse.world;
 
 import Pepse.util.ColorSupplier;
+import Pepse.util.PerlinNoise;
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 
 import java.awt.*;
+import java.util.Random;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
 
 public class Terrain {
     private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
@@ -15,6 +20,7 @@ public class Terrain {
     private int groundLayer;
     private float groundHeightAtX0;
     private Vector2 windowDimensions;
+    private PerlinNoise perlinNoise;
     public Terrain(GameObjectCollection gameObjects,
                    int groundLayer,
                    Vector2 windowDimensions){
@@ -23,26 +29,33 @@ public class Terrain {
         this.groundLayer = groundLayer;
         this.groundHeightAtX0 = windowDimensions.y() * (2f/3);
         this.windowDimensions = windowDimensions;
+        double seed = new Random().nextGaussian() * 255;
+//        double seed = new Random().nextInt();
+
+        System.out.println("seed: " + seed);
+        perlinNoise = new PerlinNoise(seed);
     }
 
     public float groundHeightAt(float x) {
-        return groundHeightAtX0;
+        float v = perlinNoise.noise(x);
+        System.out.println("v: " + v);
+        return abs(v) * (this.windowDimensions.y() - 150);
     }
 
     public void createInRange(int minX, int maxX) {
         RectangleRenderable rectangleRenderablenew = new RectangleRenderable(ColorSupplier.
                 approximateColor(BASE_GROUND_COLOR));
-        float block_size = Block.SIZE;
-        for (int x = minX; x <= maxX; x+=block_size) {
-            float groundHeightAtX = groundHeightAt(x);
-            double floor = Math.floor(groundHeightAt(groundHeightAtX) / Block.SIZE) * Block.SIZE;
+        for (int x = minX; x <= maxX; x += Block.SIZE) {
+//            float groundHeightAtX = groundHeightAt(x);
+//            System.out.println(groundHeightAtX);
+            double floor = Math.floor(groundHeightAt(x) / Block.SIZE) * Block.SIZE;
+            System.out.println("floor: "+ floor);
             while (floor <= this.windowDimensions.y()){
                 GameObject block = new Block(new Vector2(x,(int)floor),rectangleRenderablenew);
                 block.setTag("ground");
                 this.gameObjects.addGameObject(block);
                 floor += block.getDimensions().y();
             }
-
         }
     }
 }
