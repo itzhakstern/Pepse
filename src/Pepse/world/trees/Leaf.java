ad59233b7e0c129d0c62435ba1aa897d30248e7c
david96 <rotenberg.david@mail.huji.ac.il>
2022-01-11 00:35:41 +0200

    add class

--- src/Pepse/world/trees/Leaf.java
+++ src/Pepse/world/trees/Leaf.java
@@ -4,6 +4,7 @@ import Pepse.util.ColorSupplier;
 import Pepse.world.Block;
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
+import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 
@@ -30,6 +31,18 @@ public class Leaf {
                         RectangleRenderable(ColorSupplier.
                         approximateColor(BASE_GROUND_COLOR));
                 GameObject leaf = new Block(new Vector2(i ,j),rectangleRenderablenew);
+
+                new Transition<Float>(
+                        leaf, //the game object being changed
+                        (angel) -> leaf.renderer().setRenderableAngle(angel),  //the method to call
+                        -10f,    //initial transition value
+                        10f,   //final transition value
+                        Transition.LINEAR_INTERPOLATOR_FLOAT,  //use a cubic interpolator
+                        2,   //transtion fully over half a day
+                        Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                        null);
+//                leaf.renderer().setRenderableAngle(angle);
+//                leaf.setDimensions(dimensionsAsVector2);
                 gameObjects.addGameObject(leaf);
             }
         }
